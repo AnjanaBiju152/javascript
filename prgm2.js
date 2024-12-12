63c035c491b608f221f5ab1c4c1d17f1dc43daaa
AnjanaBiju152 <anjana.biju200@gmail.com>
2024-12-12 11:07:28 +0530

    javascript

---
+++ prgm2.js
@@ -0,0 +1,81 @@
+console.log("DATA TYPES");
+console.log("-------------------------------");
+console.log(typeof ("hi"));
+console.log(typeof(true));
+console.log(typeof(123));
+
+var firstName="anjana";
+console.log(typeof(firstName));
+
+
+var lastName;
+console.log(".............");
+console.log(typeof(lastName));
+
+var x=null;
+console.log(typeof(x));
+console.log(typeof(5.6));
+console.log("-------------------------------");
+
+console.log("basic operator");
+console.log(5+1);
+console.log(5-1);
+console.log(5*1);
+console.log(5/1);
+console.log(5%2);
+console.log("-------------------------------");
+var num1=1;
+var num2=8;
+console.log(num1==num2);
+console.log("-------------------------------");
+var num1=6;
+var num2=6;
+console.log(num1==num2);
+
+console.log("-------------------------------");
+var num1=6;
+var num2=6;
+console.log(num1===num2);
+
+
+console.log("-------------------------------");
+var num1=6;
+var num2="anjana";
+console.log(num1===num2);
+console.log("-------------------------------");
+
+var num1=6;
+var num2="anjana";
+console.log(num1!=num2);
+
+console.log("-------------------------------");
+var num1=6;
+var num2="anjana";
+console.log(num1!==num2);
+
+console.log("-------------------------------");
+
+var mark= 30;
+console.log(++mark);
+console.log(--mark);
+
+console.log("-------------------------------");
+
+
+var x = "hi";
+var y = "hello"
+console.log(x+y);
+
+var m1 =1;
+var m2 ="2";
+console.log(m1+m2);
+
+var companyName ="TCS";
+console.log("i am working in",companyName);
+console.log(`i am working in ${companyName}`);
+
+
+
+var userName="anjana";
+var companyName="TCS";
+console.log(`my name is ${userName} and working in ${companyName}`);
